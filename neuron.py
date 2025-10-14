7aeb8f0aaf397e14be521756f1e36091e0ea54f3
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-10-14 10:49:41 +0200

    basic layot

---
+++ neuron.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class baseNeuron(ABC):
+    @abstractmethod
+    def getValue(self) -> float:
+        pass
+
+class neuron(baseNeuron):
+    def __init__(self):
+        self.trigger: float = 0.5
+        self.inputNeurons: list[baseNeuron] = []
+        self.inputWeights: list[float] = []
+    
+    def getValue(self):
+        ans = 0
+        i = 0
+        for i, n in enumerate(inputNeurons):
+            ans += n.getValue() * self.inputWeights[i]
+            i += 1
+        return ans / i
+    
+class inputNeuron(baseNeuron):
+    def __init__(self, value: float):
+        self.value = value
+    
+    def getValue(self):
+        return self.value
